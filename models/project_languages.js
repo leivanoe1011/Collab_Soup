9a25d099bfe892b5280edb6644da0641d5c4df8f
Noe Leiva <leiva.noe.86@gmail.com>
2020-07-22 18:26:06 -0500

    added foreign key contraint to models

--- models/project_languages.js
+++ models/project_languages.js
@@ -1,7 +1,7 @@
 
 module.exports = function(sequelize, DataTypes) {
     var project_languages = sequelize.define("project_languages", {
-        project_id: {type: DataTypes.INTEGER, allowNull: false},
+        project_id: {type: DataTypes.INTEGER, allowNull: false, references: "projects", referencesKey: "id"},
         language_name: {type: DataTypes.STRING(255), allowNull: false}
     });
 
