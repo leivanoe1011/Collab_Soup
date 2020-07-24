d96532264872b05d40c210b6c909c756d754d0f4
The-Napster <connorh16@gmail.com>
2020-07-23 22:27:52 -0400

    updates to the landing page.

--- public/js/index.js
+++ public/js/index.js
@@ -97,3 +97,27 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+
+
+$(document).ready(function () {
+
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+  
+  
+  //login modal open and close.
+  $("#modal-btn").click(function () {
+      $("#login-modal").addClass("is-active")
+  })
+  $("#modalClose").click(function () {
+      $("#login-modal").removeClass("is-active")
+  })
+
+});
