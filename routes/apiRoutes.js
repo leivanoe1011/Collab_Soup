e71cd67a376a42a72a3df3ae728b610875841d85
Noe Leiva <leiva.noe.86@gmail.com>
2020-07-25 07:52:37 -0500

    created api posts and get

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -25,8 +25,68 @@ module.exports = function(app) {
         var newUser = req.body;
 
         console.log(newUser);
-        db.User.create(newUser). then(function(dbUser){
+        db.User.create(newUser).then(function(dbUser){
             res.json(dbUser);
+        })
+    });
+
+    app.post("/api/userLanguage", function(req, res){
+
+        var newLanguage = req.body;
+
+        db.User_language.create(newLanguage).then(function(dbLang){
+            res.json(dbLang);
+        })
+
+    });
+
+    app.post("/api/project", function(req, res){
+        var newProject = req.body;
+        db.Project.create(newProject).then(function(dbProject){
+            res.json(dbProject);
+        });
+    });
+
+    app.post("/api/projectLanguage", function(req, res){
+        var newProjLang = req.body;
+        db.Project_language.create(newProjLang).then(function(dbProjLang){
+            res.json(dbProjLang);
+        });
+    });
+
+    app.get("/api/userLanguage/:id", function(req, res){
+       
+        var userId = req.params.id;
+       
+        db.User_language.findAll({
+            where: {
+                user_id: userId
+            }
+        }).then(function(userLang){
+            res.json(userLang);
+        });
+    });
+
+    app.get("/api/projLanguage/:id", function(req, res){
+
+        var projectId = req.params.id;
+        db.Project_language.findAll({
+            where: {
+                project_id: projectId
+            }
+        }).then(function(projLang){
+            res.json(projLang);
+        })
+    });
+
+    app.get("/api/project/:id", function(req, res){
+        var projectId = req.params.id;
+        db.Project.findAll({
+            where: {
+                id: projectId
+            }
+        }).then(function(dbProject){
+            res.json(dbProject);
         })
     })
 }
