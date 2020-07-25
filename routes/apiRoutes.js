a0f518bc35c391fdf414b2fde5658bff3b355ff2
Noe Leiva <leiva.noe.86@gmail.com>
2020-07-25 12:10:48 -0500

    created post and get api routes for the user project sequelize db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,6 +14,7 @@ module.exports = function(app) {
     });
     // End of get by Email
 
+
     app.post("/api/user", function(req, res){
         
         console.log("In api post");
@@ -26,8 +27,12 @@ module.exports = function(app) {
         })
     });
 
+
     app.post("/api/userLanguage", function(req, res){
 
+        // The ID will get passed down here
+        // have to make sure we bring in the User_ID
+        // Our association will make that connection of User_ID to the foreign key
         var newLanguage = req.body;
 
         db.User_language.create(newLanguage).then(function(dbLang){
@@ -36,6 +41,7 @@ module.exports = function(app) {
 
     });
 
+
     app.post("/api/project", function(req, res){
         var newProject = req.body;
         db.Project.create(newProject).then(function(dbProject){
@@ -43,12 +49,42 @@ module.exports = function(app) {
         });
     });
 
+
     app.post("/api/projectLanguage", function(req, res){
         var newProjLang = req.body;
         db.Project_language.create(newProjLang).then(function(dbProjLang){
             res.json(dbProjLang);
         });
     });
+
+
+    app.post("/api/userProject", function(req, res){
+        var newUserProj = req.body;
+        db.User_project.create(newUserProj).then(function(dbUserProject){
+            res.json(dbUserProject);
+        });
+    });
+
+
+    // get all projects by User
+    app.get("/api/userProject/:id", function(req, res){
+        var userId = req.params.id;
+       
+        db.User_project.findAll({ where: { user_id: userId } }).then(function(dbUserProject){
+            res.json(dbUserProject);
+        });
+    });
+
+
+    // get all users by Project
+    app.get("/api/projectUser/:id", function(req, res){
+        var projectId = req.params.id;
+       
+        db.User_project.findAll({ where: { project_id: projectId } }).then(function(dbProjectUser){
+            res.json(dbProjectUser);
+        });
+    });
+
 
     app.get("/api/userLanguage/:id", function(req, res){
        
@@ -59,19 +95,21 @@ module.exports = function(app) {
         });
     });
 
+
     app.get("/api/projLanguage/:id", function(req, res){
 
         var projectId = req.params.id;
         db.Project_language.findAll({ where: {project_id: projectId} } ).then(function(projLang){
             res.json(projLang);
-        })
+        });
     });
+
 
     app.get("/api/project/:id", function(req, res){
         var projectId = req.params.id;
         db.Project.findAll({ where: { id: projectId } }).then(function(dbProject){
             res.json(dbProject);
-        })
-    })
+        });
+    });
 }
 
