70b6cd41ccca32a05b8876932a375e3a46b0edf3
The-Napster <connorh16@gmail.com>
2020-07-25 13:49:25 -0400

    made the user account creation form functional

--- public/js/index.js
+++ public/js/index.js
@@ -1,142 +1,45 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
-
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  }
-};
-
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
-
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
-
-
 $(document).ready(function () {
 
 
   // Check for click events on the navbar burger icon
   $(".navbar-burger").click(function () {
 
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
 
   });
-  
-  
+
+
   //login modal open and close.
   $("#modal-btn").click(function () {
-      $("#login-modal").addClass("is-active")
+    $("#login-modal").addClass("is-active")
   })
   $("#modalClose").click(function () {
-      $("#login-modal").removeClass("is-active")
+    $("#login-modal").removeClass("is-active")
   })
 
-  var $accountEmail = $("#accountEmail").val()
-  var $emailConfirm = $("#emailConfirm").val()
-  var $accountPassword = $("#accountPassword").val()
-  var $passwordConfirm = $("#passwordConfirm").val()
 
-  var accountObj = {
-    accountEmail: $accountEmail,
-    accountPassword: $accountPassword
-  }
 
-  $("#accountCreate").click(function(){
-    if($accountEmail === $emailConfirm &&  $accountPassword === $passwordConfirm){
-      $("#accountConfirm").html("Successful!")
+
+  $("#accountCreate").click(function () {
+    var $accountEmail = $("#accountEmail").val();
+    var $emailConfirm = $("#emailConfirm").val();
+    var $accountPassword = $("#accountPassword").val();
+    var $passwordConfirm = $("#passwordConfirm").val();
+
+    var accountObj = {
+      user_name: "123",
+      email: $accountEmail,
+      password: $accountPassword
+    }
+
+    if ($accountEmail === $emailConfirm && $passwordConfirm === accountPassword) {
+      $("#accountConfirm").html("Successful!");
       $.post("/api/user", accountObj)
-    } else{
-      $("#accountConfirm").html("Passwords and emails do not match properly!")
-      return
+    } else {
+      $("#accountConfirm").html("Passwords and emails do not match properly!");
+      return false
     }
   })
 
