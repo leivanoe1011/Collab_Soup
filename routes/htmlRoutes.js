89cbdb2485840e00547a7bb5168cccfcfbd32e2f
The-Napster <connorh16@gmail.com>
2020-07-27 19:42:50 -0400

    removed useless functions.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,20 +4,14 @@ var db = require("../models");
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
       });
-    });
   });
 
   app.get("/creation", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-      res.render("creation", {
-        msg: "account creation",
-        examples: dbExamples
-      });
+    res.render("creation", {
+      examples: dbExamples
     });
   });
 
@@ -25,17 +19,6 @@ module.exports = function (app) {
     res.render("about", {
       msg: "about page"
     });
-  });
-
-
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-
-    res.render("creation", {
-      example: account
-    });
-
   });
 
   // Render 404 page for any unmatched routes
