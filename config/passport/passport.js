a64203527f54075aa3bf784f6c31b7f7a06e6f67
Noe Leiva <leiva.noe.86@gmail.com>
2020-07-28 20:59:21 -0500

    added local signin to passport js

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -82,5 +82,77 @@ module.exports = function(passport, user){
         else{
             done(user.errors, null);
         }
-    })
+    });
+
+
+    //LOCAL SIGNIN
+    passport.use('local-signin', new LocalStrategy(
+
+        {
+    
+            // by default, local strategy uses username and password, we will override with email
+    
+            usernameField: 'email',
+    
+            passwordField: 'password',
+    
+            passReqToCallback: true // allows us to pass back the entire request to the callback
+    
+        },
+    
+    
+        function(req, email, password, done) {
+    
+            var User = user;
+    
+            // compares the password entered with the bCrypt comparison 
+            // method since we stored our password with bcrypt
+            var isValidPassword = function(userpass, password) {
+    
+                return bCrypt.compareSync(password, userpass);
+    
+            }
+    
+            User.findOne({
+                where: {
+                    email: email
+                }
+            }).then(function(user) {
+    
+                if (!user) {
+    
+                    return done(null, false, {
+                        message: 'Email does not exist'
+                    });
+    
+                }
+    
+                if (!isValidPassword(user.password, password)) {
+    
+                    return done(null, false, {
+                        message: 'Incorrect password.'
+                    });
+    
+                }
+    
+    
+                var userinfo = user.get();
+                return done(null, userinfo);
+    
+    
+            }).catch(function(err) {
+    
+                console.log("Error:", err);
+    
+                return done(null, false, {
+                    message: 'Something went wrong with your Signin'
+                });
+    
+            });
+    
+    
+        }
+    
+    ));
+
 }
