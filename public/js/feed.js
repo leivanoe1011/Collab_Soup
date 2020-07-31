ca2dd61d5d17aa2cbc0c25046a947916ab1b851e
The-Napster <connorh16@gmail.com>
2020-07-30 23:27:00 -0400

    minor changes to feed page code. No its not just a space noe.

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,34 +1,25 @@
 $(document).ready(function () {
-
     // FEED PAGE
     $.get("/api/projectAll", function (response) {
-        var project = response
+        project = response;
 
-
-        console.log(project)
+        console.log(project);
 
 
         var createBox = () => {
-            var feedDiv = $("#feedContent");
-            var column = $("<div class='column'>");
-            var box = $("<div class='box'>");
-            var content = $("<div class='content'>");
-
-            var projName = $("<p>")
+            const feedDiv = $("#feedContent");
+            let column = $("<div class='column'>");
+            let box = $("<div class='box'>");
+            let content = $("<div class='content'>");
+            let projName = $("<p>")
 
             projName.html('Project Name: ' + project[i].project_name);
-            
 
-
-
-            content.append(projName)
-
-            box.append(content)
-
-            column.append(box)
-
-            feedDiv.append(column)
-        }
+            content.append(projName);
+            box.append(content);
+            column.append(box);
+            feedDiv.append(column);
+        };
 
 
 
