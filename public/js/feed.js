a3e087caf20a276ea26b647dcc2554e95ec1f2f9
The-Napster <connorh16@gmail.com>
2020-08-01 11:13:35 -0400

    added some feed code.

--- public/js/feed.js
+++ public/js/feed.js
@@ -6,23 +6,23 @@ $(document).ready(function () {
         console.log(project);
 
 
-        var createBox = () => {
+        const createBox = () => {
             const feedDiv = $("#feedContent");
             let column = $("<div class='column'>");
             let box = $("<div class='box'>");
             let content = $("<div class='content'>");
-            let projName = $("<p>")
+            let projName = $("<p>");
+            let projDesc = $("<p>");
 
             projName.html('Project Name: ' + project[i].project_name);
+            projDesc.html('Project description: ' + project[i].project_description);
 
-            content.append(projName);
+
+            content.append(projName, projDesc);
             box.append(content);
             column.append(box);
             feedDiv.append(column);
         };
-
-
-
 
         for (var i = 0; i < project.length; i++) {
             createBox();
